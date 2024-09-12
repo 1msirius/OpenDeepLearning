694e7bdf0fe9af37409d06225984e81ded66ae2f
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-09-13 01:31:20 +0530

    update

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,19 +2,11 @@ import type { MDXComponents } from "mdx/types";
 import defaultComponents from "fumadocs-ui/mdx";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
-import {
-  Adsense,
-  Error,
-  Info,
-  LinkButton,
-  Tip,
-  Warning,
-} from "@/components/mdx";
+import { Error, Info, LinkButton, Tip, Warning } from "@/components/mdx";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
-    Adsense,
     Error,
     Info,
     LinkButton,
