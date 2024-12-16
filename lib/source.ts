1134fff2fca08ad65f20e71c5f8e5c9e9fde8815
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-12-16 17:23:34 +0530

    Add react icons

--- lib/source.ts
+++ lib/source.ts
@@ -1,17 +1,25 @@
 import { docs, meta } from "@/.source";
 import { createMDXSource } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
-import { icons } from "lucide-react";
 import { createElement } from "react";
 import IconContainer from "@/components/icon";
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
+import * as LuIcons from "react-icons/lu";
+
+const allIcons = {
+  ...FaIcons,
+  ...SiIcons,
+  ...LuIcons,
+};
 
 export const source = loader({
   baseUrl: "/docs",
   source: createMDXSource(docs, meta),
   icon(icon) {
-    if (icon && icon in icons)
-      return createElement(IconContainer, {
-        icon: icons[icon as keyof typeof icons],
-      });
+    if (icon && icon in allIcons) {
+      const IconComponent = allIcons[icon as keyof typeof allIcons];
+      return createElement(IconContainer, { icon: IconComponent });
+    }
   },
 });
